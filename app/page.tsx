af39624a48196a3995dc6f1d8625a1d21b0bb129
Alexis Valentin <alexis.jean.valentin@gmail.com>
2025-12-14 17:41:21 +0100

    feat: add navigation on homepage

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 
 const HomePage = () => {
 	return (
-		<main className="flex items-center justify-center min-h-[calc(100vh-200px)] px-6 md:px-12">
+		<main className="flex flex-col items-center justify-center min-h-[calc(100vh-200px)] px-6 md:px-12">
 			<Link
 				href="/projets"
 				className="relative group block w-full max-w-4xl aspect-4/3 overflow-hidden"
@@ -17,6 +17,18 @@ const HomePage = () => {
 				/>
 				<div className="absolute inset-0 bg-black/0 group-hover:bg-black/10 transition-colors duration-300" />
 			</Link>
+
+			<nav className="flex gap-8 mt-8 md:mt-12">
+				<Link href="/a-propos" className="nav-link nav-link-inactive uppercase font-extrabold hover:text-black">
+					A propos
+				</Link>
+				<Link href="/projets" className="nav-link nav-link-inactive uppercase font-extrabold hover:text-black">
+					Projets
+				</Link>
+				<Link href="/contact" className="nav-link nav-link-inactive uppercase font-extrabold hover:text-black">
+					Contact
+				</Link>
+			</nav>
 		</main>
 	);
 };
